925dc37057669c73d6dcae1889474280448bb427
oszkar0 <oskszysiak@gmail.com>
2023-11-04 12:16:39 +0100

    write resize_images function

--- api/pizza-image-generator/image_generating_and_processing.py
+++ api/pizza-image-generator/image_generating_and_processing.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import cv2
 
 model = tf.keras.models.load_model('static/generator_model')
 
@@ -9,3 +10,13 @@ def generate_images(num_images):
     generated_images = model.predict(random_noise, verbose=0)
     generated_images = (generated_images * 127.5 + 127.5).astype('uint8')
     return generated_images
+
+
+def resize_images(images, size):
+    resized_images = []
+
+    for image in images:
+        image = cv2.resize(image, size)
+        resized_images.append(image)
+
+    return resized_images
