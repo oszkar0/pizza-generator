987b6da27c31308858c8a6d00acd76adc683bf76
oszkar0 <oskszysiak@gmail.com>
2023-11-04 12:22:00 +0100

    write b64_encode_images function

--- api/pizza-image-generator/image_generating_and_processing.py
+++ api/pizza-image-generator/image_generating_and_processing.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import cv2
+import io
+import base64
 
 model = tf.keras.models.load_model('static/generator_model')
 
@@ -20,3 +22,15 @@ def resize_images(images, size):
         resized_images.append(image)
 
     return resized_images
+
+
+def b64_encode_images(images):
+    encoded_images = []
+
+    for image in images:
+        buffer = io.BytesIO()
+        tf.keras.utils.save_img(buffer, image, data_format='channels_last', file_format='png')
+        image_data = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        encoded_images.append(image_data)
+
+    return encoded_images
